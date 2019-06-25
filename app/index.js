0f843769255e297a575922f1789b558ac70b1472
Devashish Patel <hello@devashishpatel.com>
2019-06-24 20:23:07 -0400

    initial repo

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/Popular";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  // React Element,
+  <App />,
+  // Where to render to the Element to
+  document.getElementById("app")
+);
