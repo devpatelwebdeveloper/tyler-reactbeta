6d52f27442a5449b3ae93a9adb608ce77b938334
Devashish Patel <hello@devashishpatel.com>
2019-06-26 14:29:13 -0400

    PropTypes

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,11 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-class Popular extends React.Component {
+function LangaugesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+  return (
+    <ul className="flex-center">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => onUpdateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LangaugesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+};
+
+export default class Popular extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       selectedLanguage: "All"
     };
+
     this.updateLanguage = this.updateLanguage.bind(this);
   }
   updateLanguage(selectedLanguage) {
@@ -14,26 +41,15 @@ class Popular extends React.Component {
     });
   }
   render() {
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    const { selectedLanguage } = this.state;
+
     return (
-      <ul className="flex-center">
-        {languages.map(language => (
-          <li key={language}>
-            <button
-              className="btn-clear nav-link"
-              style={
-                language === this.state.selectedLanguage
-                  ? { color: "rgb(187, 46, 31)" }
-                  : null
-              }
-              onClick={() => this.updateLanguage(language)}>
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LangaugesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+      </React.Fragment>
     );
   }
 }
-
-export default Popular;
